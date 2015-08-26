9be1e516dc715bff7054e2f09001e69d74988067
Максим Самойлов <samoilowmaxim@gmail.com>
2015-08-26 23:03:20 +0600

    feat(order): add order__main__short

--- app/components/order/index.jsx
+++ app/components/order/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import './styles.css';
+
 export default class Order extends React.Component {
 	render() {
 		return (
-			<div>
-				Order
+			<div className='order'>
+				<div className='order__main'>
+					<div className='order__main-bg' />
+					<div className='order__main__short'>
+						<div id='posAirship' className='order__main__short-block'>
+							<div className='order__main__short__airship' />
+							<div className='order__main__short__text-block' />
+							<a href='#faq' className='order__main__short__more blue-link big-blue-link'>подробности</a>
+						</div>
+					<div className='order__main__content'>
+					</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
