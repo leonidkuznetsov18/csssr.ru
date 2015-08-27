8f15c0dd1ff7489332129a3e8208652b03f6cbee
Максим Самойлов <samoilowmaxim@gmail.com>
2015-08-27 16:43:46 +0600

    feat(order): move text to order-content.json

--- app/components/order-content/index.jsx
+++ app/components/order-content/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import OrderForm from 'components/order-form'
+import Section from 'components/section'
 
 import './styles.css';
+
+const data = require('data/order-content.json');
+
 
 export default class OrderShort extends React.Component {
 	render() {
 		return (
 			<div className='order__main__content'>
-				<h1 className='order__main__content__title'>Вёрстка проекта в CSSSR</h1>
-				<p className='order__main__content__desc'>Вы&nbsp;собираетесь заказать волшебное превращение макетов в&nbsp;шаблоны.</p>
-				<p className='order__main__content__text comment'>
-					Обратите внимание, что после получения заказа мы&nbsp;свяжемся с&nbsp;вами для уточнения нюансов.
-					Пожалуйста, учитывайте, что для согласования всех условий заказа,
-					а&nbsp;также расчёта стоимости и&nbsp;сроков выполнения проекта потребуется некоторое время.
-				</p>
+				<Section
+					title={data.title}
+					description={data.description}
+				/>
+				<p
+					className='order__main__content__text comment'
+					dangerouslySetInnerHTML={{__html: data.text}}
+				/>
 				<OrderForm />
 			</div>
 		);
