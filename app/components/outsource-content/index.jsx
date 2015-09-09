c5aec613ee05aaa2c9fede2c33c947cc386bf4cd
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-09 16:05:44 +0300

    feat(outsource): add events block

---
+++ app/components/outsource-content/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './styles.css';
+
+export default class OutsourceContent extends React.Component {
+
+	render() {
+		const data = this.props.data;
+
+		const events = data.events.cols.map((event, i) => {
+			return (
+				<div key={i} className='outsource__cols-item'>
+					<div className={`outsource__cols-img outsource__cols-${event.class}`} />
+					<h2 className='outsource__cols-title'>{event.title}</h2>
+					<p className='outsource__cols-text'>{event.text}</p>
+				</div>
+			)
+		});
+
+		return (
+			<div className='outsource__content'>
+				<div className='outsource__events'>
+					<h2 className='outsource__subtitle outsource__events-title'>
+						{data.events.title}
+					</h2>
+
+					<div className='outsource__cols'>
+						{events}
+						<img
+							className='outsource-power__cloud outsource-power__cloud_top'
+							src={require('images/background/cloudx3.svg')}
+							alt='cloud'
+							height={139}
+							width={212}
+						/>
+
+					</div>
+
+				</div>
+			</div>
+		);
+	}
+}
--- app/components/outsource/index.jsx
+++ app/components/outsource/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import OutsourceContent from 'components/outsource-content'
 
 import './styles.css';
+
+const data = require('data/outsource.json');
 
 export default class Outsource extends React.Component {
 
@@ -16,6 +19,7 @@ export default class Outsource extends React.Component {
 					<span className='outsource__title-left'>Frontend</span>
 					<span className='outsource__title-right'>аутсорсинг</span>
 				</h1>
+				<OutsourceContent data={data} />
 			</div>
 		);
 	}
