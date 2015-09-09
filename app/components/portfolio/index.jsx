3016b6cc76de01466dbf440c6180ddd7947bbf7b
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-09 11:21:48 +0300

    feat(portfolio): add portfolio title, text and rocket image

--- app/components/portfolio/index.jsx
+++ app/components/portfolio/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import './styles.css';
+
+const data = require('data/portfolio.json');
+
 export default class Portfolio extends React.Component {
 	render() {
 		return (
-			<div>
-				Portfolio
+			<div className='portfolio'>
+				<div className='portfolio-head'>
+					<div className='portfolio-slogan' />
+					<div className='portfolio-rocket' />
+				</div>
+				<div className='portfolio-main'>
+					<div className='portfolio-title'>{data.title}</div>
+					<div className='portfolio-desc'>
+						{data.info}
+					</div>
+				</div>
 			</div>
 		);
 	}
