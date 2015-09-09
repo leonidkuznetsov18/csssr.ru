f7011a43f5a008cb50130aa3898d6ea265d8b964
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-09 15:56:02 +0300

    feat: add calcDate helper

---
+++ app/helpers/calcDate.js
@@ -0,0 +1,55 @@
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+export default function calcDate(date) {
+	const current = new Date();
+	let year = current.getYear() - date.getYear();
+	let month = current.getMonth() - date.getMonth();
+	let day = current.getDate() - date.getDate();
+	let time = [];
+
+	if (day < 0) {
+		month--;
+		day += daysInMonth[(current.getMonth() - 1 + 12) % 12];
+	}
+
+	if (month < 0) {
+		year--;
+		month += 12;
+	}
+
+	if (year > 0) {
+		time.push(year + ' ' + plural(year, 'год', 'года', 'лет'));
+	}
+
+	if (month > 0) {
+		time.push(month + ' ' + plural(month, 'месяц', 'месяца', 'месяцев'));
+	}
+
+	if (day > 0) {
+		time.push(day + ' ' + plural(day, 'день', 'дня', 'дней'));
+	}
+
+	const string = time.reduce(function(prev, current, index, array) {
+		let prefix = '';
+		if (index > 0) {
+			prefix = ', ';
+		}
+		if (index == array.length - 1 && index !== 0) {
+			prefix = ' и ';
+		}
+		return prev + prefix + current;
+	}, '')
+
+	return string;
+}
+
+function plural(num, single, some, multiple) {
+	if (num === 1) {
+		return single;
+	} else if (num < 5) {
+		return some;
+	} else {
+		return multiple;
+	}
+}
+
