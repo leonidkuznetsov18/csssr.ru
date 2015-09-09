5755e7eef002c1e59598efe6703a80fcfb4e744d
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-09 12:56:31 +0300

    feat(outsource): add titles

--- app/components/outsource/index.jsx
+++ app/components/outsource/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import './styles.css';
+
 export default class Outsource extends React.Component {
+
+	componentDidMount() {
+		document.title = 'CSSSR — фронтенд аутсорсинг. Что угодно с помощью HTML, CSS, JavaScript.'
+	}
+
+
 	render() {
 		return (
-			<div>
-				Outsource
+			<div className='outsource'>
+				<h1 className='outsource__title'>
+					<span className='outsource__title-left'>Frontend</span>
+					<span className='outsource__title-right'>аутсорсинг</span>
+				</h1>
 			</div>
 		);
 	}
