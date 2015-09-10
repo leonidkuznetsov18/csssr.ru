a4f92e917020803004e5581dc8c6c44035539811
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-10 12:22:37 +0300

    feat(outsource): add use__title, images and use__how

--- app/components/outsource-use/index.jsx
+++ app/components/outsource-use/index.jsx
@@ -6,8 +6,39 @@ import './styles.css';
 export default class OutsourceUse extends React.Component {
 
 	render() {
+		const cloud = require('images/background/cloudx3.svg');
 		return (
-			<div>content</div>
+			<div className="outsource-use">
+				<h2 className="outsource-use__title">
+					<div className="outsource-use__title-text">Идеи Применения</div>
+				</h2>
+
+				<img
+					className='outsource-use__cloud outsource-use__cloud_left'
+					src={cloud}
+				/>
+				<img
+					className='outsource-use__cloud outsource-use__cloud_right'
+					src={cloud}
+				/>
+
+				<div className="outsource-use__how">
+					<h3 className="outsource-use__subtitle">
+						как применить
+						<br />
+						наш сервис
+						<br />
+						в вашем бизнесе?
+					</h3>
+					<div className="outsource-use__text">
+						— Что такое «Frontend аутсосринг», как это может быть полезно?
+						<br />
+						— Ок, сейчас объясним наглядно...
+					</div>
+
+				</div>
+
+			</div>
 		);
 	}
 }
