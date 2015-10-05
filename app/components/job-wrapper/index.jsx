f93af4581134d668eab9d210d8f259f1d978e65d
Максим Самойлов <samoilowmaxim@gmail.com>
2015-10-05 13:10:52 +0300

    feat(job-wrapper): add basic markup and styles

--- app/components/job-wrapper/index.jsx
+++ app/components/job-wrapper/index.jsx
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
 import './styles.css';
 
@@ -6,14 +7,23 @@ import './styles.css';
 export default class JobsWrapper extends React.Component {
 
 	static propTypes = {
-		children: PropTypes.node.isRequired
+		children: PropTypes.node.isRequired,
+		location: PropTypes.object
 	}
 
 	render() {
+		const page = this.props.location.pathname.match(/[^\/]*$/)[0];
+
+		// TODO: get pageName from json file
+		const pageName = page;
+
 		return (
 			<div>
-				<span>JobWrapper</span>
-				{this.props.children}
+				<div className='job-wrapper-nav'>
+					<Link to='/jobs' className='job-wrapper-nav__link'>Все вакансии</Link>
+					<span className='job-wrapper-nav__current'> / {pageName}</span>
+				</div>
+				<div className='job-wrapper__main'>{this.props.children}</div>
 			</div>
 		);
 	}
