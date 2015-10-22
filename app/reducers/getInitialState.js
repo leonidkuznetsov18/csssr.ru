e6443d5a620a0615dc2285e02251c748b592f9b9
Максим Самойлов <samoilowmaxim@gmail.com>
2015-10-22 20:18:45 +0300

    fix(order-form): rewrite initial state from yaml to javascript because yaml doesn't support functions inside

--- app/reducers/getInitialState.js
+++ app/reducers/getInitialState.js
@@ -1,16 +1,24 @@
 import FormItem from 'helpers/FormItem';
-import jobs from './jobs.yml';
-
-const newJobs = jobs;
+import jobs from './jobs.initial-state';
+import order from './order.initial-state';
 
 for (const key in jobs) if (jobs.hasOwnProperty(key)) {
-	newJobs[key] = jobs[key];
-	const form = newJobs[key].form;
+	jobs[key] = jobs[key];
+	const form = jobs[key].form;
 	for (const k in form) if (form.hasOwnProperty(k)) {
-		const item = form[k];
-		form[k] = new FormItem(item.value, item.validate, item.showError);
+		form[k] = new FormItem(form[k].value, form[k].validate, form[k].showError);
 	}
 }
 
-export {newJobs as jobs};
-export order from './order.yml';
+
+const contacts = order.form.contacts;
+for (const key in contacts) if (contacts.hasOwnProperty(key)) {
+	contacts[key] = new FormItem(
+		contacts[key].value,
+		contacts[key].validate,
+		contacts[key].showError
+	);
+}
+
+
+export {jobs, order};
