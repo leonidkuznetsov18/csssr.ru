4dfd2d0933a523c47db86f681fc8874140134023
Aleksandr Petrov <dzhiriki@gmail.com>
2015-11-12 18:19:38 +0300

    refactor(menu): replace menu with stateless component

--- app/components/menu-item/index.jsx
+++ app/components/menu-item/index.jsx
@@ -1,24 +1,34 @@
 import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
 import { Link } from 'react-router';
 
 import './styles.css';
 
-export default class MenuItem extends React.Component {
-	static propTypes = {
-		href: PropTypes.string,
-		text: PropTypes.string
-	}
+export default function MenuItem({href, children, active, component}) {
+	const Component = component;
+	const blockClass = cx({
+		'menu-item': true,
+		'menu-item_state_active': active
+	});
 
-	render() {
-		return (
-			<Link
-				className='menu-item'
-				activeClassName='menu-item_state_active'
-				to={this.props.href}
-			>
-				{this.props.text}
-				<span className='menu-item__arrow'/>
-			</Link>
-		);
-	}
+	return (
+		<Component
+			className={blockClass}
+			activeClassName='menu-item_state_active'
+			to={href}
+		>
+			{children}
+			<span className='menu-item__arrow'/>
+		</Component>
+	);
+}
+
+MenuItem.defaultProps = {
+	component: Link
+}
+
+MenuItem.propTypes = {
+	href: PropTypes.string,
+	children: PropTypes.string
 }
--- app/components/menu/index.jsx
+++ app/components/menu/index.jsx
@@ -1,6 +1,5 @@
 import React , {PropTypes} from 'react';
 import MenuItem from 'components/menu-item';
-import cx from 'classnames';
 
 import './styles.css';
 
@@ -26,38 +25,32 @@ const menu = [
 		href: '/portfolio'
 	}
 ];
-const renderMenuItem = item => (
-			<li className='menu__item' key={item.href}>
-				<MenuItem {...item} />
-			</li>
-		);
 
-export default class Menu extends React.Component {
-	static propTypes = {
-		open: PropTypes.func,
-		active: PropTypes.bool
-	}
-
-	render() {
-		const contactClass = cx ({
-			'menu-item': true,
-			'menu-item_state_active': this.props.active
-		});
-		return (
-			<nav className='menu'>
-				<ul className='menu__list'>
-					<li className='menu__item menu__item_fix'/>
-					{menu.map(renderMenuItem)}
-					<li className='menu__item' onClick={this.props.open}>
-						<a className={contactClass}>
-							Контакты
-							<span className='menu-item__arrow'/>
-						</a>
+export default function Menu() {
+	return (
+		<nav className='menu'>
+			<ul className='menu__list'>
+				<li className='menu__item menu__item_fix'/>
+				{menu.map(item => (
+					<li className='menu__item' key={item.href}>
+						<MenuItem href={item.href}>
+							{item.text}
+						</MenuItem>
 					</li>
-					<li className='menu__item menu__item_fix'/>
-				</ul>
-				<div className='menu__shadow'/>
-			</nav>
-		);
-	}
+				))}
+				<li className='menu__item' onClick={this.props.open}>
+					<MenuItem component='a' active={this.props.active}>
+						Контакты
+					</MenuItem>
+				</li>
+				<li className='menu__item menu__item_fix'/>
+			</ul>
+			<div className='menu__shadow'/>
+		</nav>
+	);
+}
+
+Menu.propTypes = {
+	open: PropTypes.func,
+	active: PropTypes.bool
 }
