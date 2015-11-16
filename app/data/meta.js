b64b6b365da11f010c5bf981c3a558805e2c4afe
Aleksandr Petrov <dzhiriki@gmail.com>
2015-11-16 11:36:58 +0300

    fix(meta): fix lint error

--- app/data/meta.js
+++ app/data/meta.js
@@ -5,88 +5,93 @@ export const index = {
 	shareUrl: 'http://csssr.ru/',
 	shareTitle: 'CSSSR — Вёрстка и JavaScript программирование',
 	shareDescription: 'Фронтенд разработка сайтов и веб-приложений',
-	shareImage: 'http://csssr.ru/share/frontend-with-space-speed.jpg'
-}
+	shareImage: 'http://csssr.ru/share/frontend-with-space-speed.jpg',
+};
 
 export const company = {
 	...index,
 	pageTitle: 'О компании',
-	pageKeywords: 'CSSSR, ЦСССР, CSSR, ЦССР'
-}
+	pageKeywords: 'CSSSR, ЦСССР, CSSR, ЦССР',
+};
 
 export const jobs = {
 	...index,
 	pageTitle: 'Вакансии CSSSR — Удалённая работа, полная страданий, боли и отчаяния',
 	shareTitle: 'Вакансии CSSSR — Удалённая работа, полная страданий, боли и отчаяния',
 	shareUrl: `${index.shareUrl}jobs`,
-	shareImage: 'http://csssr.ru/share/remote-work.jpg'
-}
+	shareImage: 'http://csssr.ru/share/remote-work.jpg',
+};
 
 export const jobsPixelPerfectionist = {
 	...jobs,
 	pageTitle: 'Вакансия «Верстальщик пиксель-перфекционист»',
 	shareTitle: 'Вакансия «Верстальщик пиксель-перфекционист»',
 	shareDescription: 'CSSSR — Работай, где хочешь!',
-	shareUrl: `${index.shareUrl}jobs/pixel-perfectionist`
-}
+	shareUrl: `${index.shareUrl}jobs/pixel-perfectionist`,
+};
 
 export const jobsTechnicalManager = {
 	...jobs,
 	pageTitle: 'Вакансия «Менеджер-технарь»',
 	shareTitle: 'Вакансия «Менеджер-технарь»',
-	shareUrl: `${index.shareUrl}jobs/technical-manager`
-}
+	shareUrl: `${index.shareUrl}jobs/technical-manager`,
+};
 
 export const order = {
 	...index,
 	pageTitle: 'Вёрстка проекта в CSSSR',
 	pageDescription: 'Заказ вёрстки сайта из PSD-макетов в HTML с гарантией дедлайна',
-	pageKeywords: 'CSSSR, вёрстка сайта, из PSD в HTML, PSD to HTML, калькулятор вёрстки, стоимость вёрстки, заказ вёрстки, калькулятор CSSSR, ЦСССР, CSSR'
-}
+	pageKeywords: 'CSSSR, вёрстка сайта, из PSD в HTML, PSD to HTML, калькулятор вёрстки, стоимость вёрстки, заказ вёрстки, калькулятор CSSSR, ЦСССР, CSSR',
+};
 
 export const outsource = {
 	...index,
 	pageTitle: 'CSSSR — фронтенд аутсорсинг. Что угодно с помощью HTML, CSS, JavaScript.',
 	pageDescription: 'CSSSR — это фронтенд аутсорсинг: HTML, CSS, JavaScript, AngularJS',
-	pageKeywords: 'CSSSR, CSSR, css-service, вёрстка, вёрстка сайтов, вёрстка PSD в HTML, PSD to HTML, frontend аутсорсинг, фронтенд, поддержка, рефакторинг, HTML,CSS, JavaScript, JS, Angular, AngularJS'
-}
+	pageKeywords: 'CSSSR, CSSR, css-service, вёрстка, вёрстка сайтов, вёрстка PSD в HTML, PSD to HTML, frontend аутсорсинг, фронтенд, поддержка, рефакторинг, HTML,CSS, JavaScript, JS, Angular, AngularJS',
+};
 
 export const offert = {
 	...index,
 	pageTitle: 'Общие условия оказания услуг',
 	pageDescription: '',
-	pageKeywords: ''
-}
+	pageKeywords: '',
+};
 
 export const confidential = {
 	...index,
 	pageTitle: 'Положение об обработке персональных данных',
 	pageDescription: '',
-	pageKeywords: ''
-}
+	pageKeywords: '',
+};
 
 export const timeline = {
 	...index,
 	pageTitle: 'История полёта',
-	pageDescription: 'Таймлайн'
-}
+	pageDescription: 'Таймлайн',
+};
+
+export const thanks = {
+	...index,
+	pageTitle: 'Успех, товарищ!',
+};
 
 export const status403 = {
 	...index,
-	pagetitle: 'CSSSR - 403'
-}
+	pagetitle: 'CSSSR - 403',
+};
 
 export const status404 = {
 	...index,
-	pagetitle: 'CSSSR - 404'
-}
+	pagetitle: 'CSSSR - 404',
+};
 
 export const status500 = {
 	...index,
-	pagetitle: 'CSSSR - 500'
-}
+	pagetitle: 'CSSSR - 500',
+};
 
 export const status502 = {
 	...index,
-	pagetitle: 'CSSSR - 502'
-}
+	pagetitle: 'CSSSR - 502',
+};
