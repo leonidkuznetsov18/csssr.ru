df3b6e7dba077d45fa98e32e2bb27a15fe417863
Oleg Vikoultsev <vikoultsev@gmail.com>
2016-03-03 01:51:57 +0700

    feat(form-job): connect form with hr

--- config/env.staging.js
+++ config/env.staging.js
@@ -1,3 +1,3 @@
-export const HR_DOMAIN = 'http://csssr.ru';
+export const HR_DOMAIN = 'http://test-hr.csssr.ru/';
 export const CRM_URL = 'http://test-tools-back.csssr.ru/api/site/order';
 export const ORDER_MAIL = 'dzhiriki@gmail.com';
--- server/jobs/index.js
+++ server/jobs/index.js
@@ -2,7 +2,7 @@ import superagent from 'superagent';
 
 export default function (req, res) {
 	let request = superagent
-		.post(`${process.env.HR_DOMAIN}/jobs/${req.body.job}/create`)
+		.post(`${process.env.HR_DOMAIN}/api/candidates`)
 		.attach('file', req.files.file.path);
 
 	Object.keys(req.body).forEach((key) => {
