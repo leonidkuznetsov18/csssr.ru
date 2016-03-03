11e0ddb6bf0cdc1e091652c8d430de30315bb9d2
Oleg Vikoultsev <vikoultsev@gmail.com>
2016-03-03 22:25:01 +0700

    fix(job-form): fix production domain

--- config/env.production.js
+++ config/env.production.js
@@ -1,3 +1,3 @@
-export const HR_DOMAIN = 'http://test-hr.csssr.ru/';
+export const HR_DOMAIN = 'http://hr.csssr.ru/';
 export const CRM_URL = 'http://space-tools-back.csssr.ru/api/site/order';
 export const ORDER_MAIL = 'sales@csssr.io';
