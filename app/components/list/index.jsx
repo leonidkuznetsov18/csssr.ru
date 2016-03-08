47e4df5843ebf735b660dad9e72c2ad81412882a
Aleksandr Petrov <dzhiriki@gmail.com>
2016-03-08 12:42:22 +0300

    fix(list): fix bullet position

--- app/components/list/index.jsx
+++ app/components/list/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import cx from 'classNames';
 import Text from 'components/text';
 
 import './styles.css';
 
 export default function List({ items, props }) {
+	const classList = cx({
+		list__item: true,
+		list__item_size_s: props.size === 's',
+	});
+
 	return (
 		<ul {...props} className='list'>
 			{items.map((item, index) => (
-				<li className='list__item' key={index}>
+				<li className={classList} key={index}>
 					<Text {...props} indent={false}>
 						{item}
 					</Text>
@@ -19,5 +25,6 @@ export default function List({ items, props }) {
 
 List.propTypes = {
 	items: React.PropTypes.array.isRequired,
+	size: React.PropTypes.string,
 	props: React.PropTypes.object,
 };
