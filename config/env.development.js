26062bbd206ea79109c640fba2edbd99ec5f6684
Felix Exter <felixexter@users.noreply.github.com>
2016-03-09 20:42:45 +0300

    fix(config): add a new email to develop config

--- config/env.development.js
+++ config/env.development.js
@@ -1,3 +1,3 @@
 export const HR_DOMAIN = 'http://test-hr.csssr.ru/';
 export const CRM_URL = 'http://rtest-tools-back.csssr.ru/api/site/order';
-export const ORDER_MAIL = 'dzhiriki@gmail.com';
+export const ORDER_MAIL = 'dzhiriki@gmail.com, nina.smolentseva@csssr.io';
