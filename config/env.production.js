c7544fa6845c284ff1ee15dd486eb08d395ec3a0
Aleksandr Petrov <dzhiriki@gmail.com>
2016-03-16 13:04:27 +0300

    fix(env): fix variable name

--- config/env.production.js
+++ config/env.production.js
@@ -1,4 +1,4 @@
-export const SITE_DOMAIN = 'http://csssr.ru/uploads/';
+export const SITE_URL = 'http://csssr.ru/uploads/';
 export const HR_DOMAIN = 'http://hr.csssr.ru/';
 export const CRM_URL = 'http://space-tools-back.csssr.ru/api/site/order';
 export const ORDER_MAIL = 'sales@csssr.io';
