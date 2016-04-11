8bbfb0a48293eaaccf98355a695d9de552c2445c
Felix Exter <felixexter@ya.ru>
2016-04-11 03:48:15 +0300

    fix(config): add emails

--- config/env.production.js
+++ config/env.production.js
@@ -1,5 +1,9 @@
 export const SITE_URL = 'http://csssr.ru/uploads/';
 export const HR_DOMAIN = 'http://hr.csssr.ru/';
 export const CRM_URL = 'http://space-tools-back.csssr.ru/api/site/order';
-export const ORDER_MAIL = 'sales@csssr.io';
+export const ORDER_MAIL = [
+	'sales@csssr.io',
+	'nina.smolentseva@csssr.io',
+	'aleksandr.kalugin@csssr.com'
+].join(', ');
 export const NODE_ENV = 'production';
